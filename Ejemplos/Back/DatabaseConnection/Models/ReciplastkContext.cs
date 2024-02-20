63b594ab81475dd43087973f2a53812e997029b3
Pedro Ospina <Pedro.Ospina@coxautoinc.com>
2024-02-19 22:01:46 -0500

    Remove tests

--- Ejemplos/Back/DatabaseConnection/Models/ReciplastkContext.cs
+++ Ejemplos/Back/DatabaseConnection/Models/ReciplastkContext.cs
@@ -21,8 +21,6 @@ public partial class ReciplastkContext : DbContext
 
     public virtual DbSet<Test> Tests { get; set; }
 
-    public virtual DbSet<Test2> Test2s { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=reciplastk.cty0a2ewmeb5.us-east-1.rds.amazonaws.com;Database=Reciplastk;Username=postgres;Password=Admin123");
@@ -51,11 +49,6 @@ public partial class ReciplastkContext : DbContext
         modelBuilder.Entity<Test>(entity =>
         {
             entity.HasKey(e => e.Testid).HasName("test_pkey");
-        });
-
-        modelBuilder.Entity<Test2>(entity =>
-        {
-            entity.HasKey(e => e.Testid).HasName("test2_pkey");
         });
 
         OnModelCreatingPartial(modelBuilder);
